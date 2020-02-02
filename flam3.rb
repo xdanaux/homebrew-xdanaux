3fa58d97f25fc432345dd99dbe8ef9a762fdb6da
Xavier Danaux <xavier.danaux@attestorcapital.com>
2020-02-02 11:31:32 +0000

    Reflect flam3 migration from google code to github

--- flam3.rb
+++ flam3.rb
@@ -1,10 +1,9 @@
 require "formula"
 
 class Flam3 < Formula
-  homepage "http://code.google.com/p/flam3/"
-  url "https://flam3.googlecode.com/files/flam3-3.0.1.tar.gz"
-#  sha1 "8814515f2e49e034e47cf97e9d2c0e932844abb9"
-  sha256 "70810049604527a689ba172670c152b463273255e46f0f04091c867399859aa7"
+  homepage "https://github.com/scottdraves/flam3"
+  url "https://github.com/scottdraves/flam3/archive/v3.0.1.tar.gz"
+  sha256 "fc9e936fb5c6cfa05afc94cf9b991259cb9f9d40c2681d2761e5a06dd5afdc9c"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -61,26 +60,26 @@ index 4704f48..aa4d4eb 100644
  #endif
 -   int thread_status;
     int thi;
-    time_t tstart,tend;   
+    time_t tstart,tend;
     double sumfilt;
 @@ -894,7 +893,7 @@ static int render_rectangle(flam3_frame *spec, void *out,
- 
+
           /* Wait for them to return */
           for (thi=0; thi < spec->nthreads; thi++)
 -            pthread_join(myThreads[thi], (void **)&thread_status);
 +            pthread_join(myThreads[thi], (void **)0);
- 
+
           #if defined(USE_LOCKS)
           pthread_mutex_destroy(&fic.bucket_mutex);
 @@ -1025,7 +1024,7 @@ static int render_rectangle(flam3_frame *spec, void *out,
- 
+
           /* Wait for them to return */
           for (thi=0; thi < spec->nthreads; thi++)
 -            pthread_join(myThreads[thi], (void **)&thread_status);
 +            pthread_join(myThreads[thi], (void **)0);
-          
-          free(myThreads);            
- #else         
+
+          free(myThreads);
+ #else
 diff --git a/src/png.c b/src/png.c
 index 23ff489..c3d1d36 100755
 --- a/src/png.c
@@ -97,7 +96,7 @@ index 23ff489..c3d1d36 100755
 @@ -161,19 +161,20 @@ unsigned char *read_png(FILE *ifp, int *width, int *height) {
    png_set_sig_bytes (png_ptr, SIG_CHECK_SIZE);
    png_read_info (png_ptr, info_ptr);
- 
+
 -  if (8 != info_ptr->bit_depth) {
 +  if (8 != png_get_bit_depth(png_ptr, info_ptr)) {
      fprintf(stderr, "bit depth type must be 8, not %d.\n",
@@ -105,7 +104,7 @@ index 23ff489..c3d1d36 100755
 +	    png_get_bit_depth(png_ptr, info_ptr));
      return 0;
    }
- 
+
 -  *width = info_ptr->width;
 -  *height = info_ptr->height;
 +  *width = png_get_image_width(png_ptr, info_ptr);
@@ -114,7 +113,7 @@ index 23ff489..c3d1d36 100755
    p = q = malloc(4 * *width * *height);
 -  png_image = (png_byte **)malloc (info_ptr->height * sizeof (png_byte*));
 +  png_image = (png_byte **)malloc (*height * sizeof (png_byte*));
- 
+
 -  linesize = info_ptr->width;
 -  switch (info_ptr->color_type) {
 +  linesize = *width;
@@ -130,20 +129,20 @@ index 23ff489..c3d1d36 100755
 +	    color_type);
      return 0;
    }
- 
+
 -  for (y = 0 ; y < info_ptr->height ; y++) {
 +  for (y = 0 ; y < *height ; y++) {
      png_image[y] = malloc (linesize);
    }
    png_read_image (png_ptr, png_image);
    png_read_end (png_ptr, info_ptr);
- 
+
 -  for (y = 0 ; y < info_ptr->height ; y++) {
 +  for (y = 0 ; y < *height ; y++) {
      unsigned char *s = png_image[y];
 -    for (x = 0 ; x < info_ptr->width ; x++) {
 +    for (x = 0 ; x < *width ; x++) {
- 
+
 -      switch (info_ptr->color_type) {
 +      switch (color_type) {
        case PNG_COLOR_TYPE_RGB:
@@ -152,9 +151,9 @@ index 23ff489..c3d1d36 100755
 @@ -217,7 +218,7 @@ unsigned char *read_png(FILE *ifp, int *width, int *height) {
      }
    }
- 
+
 -  for (y = 0 ; y < info_ptr->height ; y++)
 +  for (y = 0 ; y < *height ; y++)
      free (png_image[y]);
    free (png_image);
-   png_destroy_read_struct (&png_ptr, &info_ptr, (png_infopp)NULL);  
+   png_destroy_read_struct (&png_ptr, &info_ptr, (png_infopp)NULL);
